18a33c5ebb3f6101e7dfcfaa1957b7dbe7acd9d8
luis-domingues <luisdomingues117@gmail.com>
2024-07-08 21:32:12 -0300

    Added Models folder with initial model class

---
+++ ApiCrud/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiCrud.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+
+        public Pessoa(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
